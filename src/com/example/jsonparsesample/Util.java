66531b6fb2a331e44f004c580115a111250ba561
Mylene King <mylene.king@gmail.com>
2013-09-25 18:49:54 +0800

    master: initial commit.

---
+++ src/com/example/jsonparsesample/Util.java
@@ -0,0 +1,10 @@
+package com.example.jsonparsesample;
+
+public class Util {
+	
+	public final static String API_PSE_ALL = "http://phisix-api.appspot.com/stocks.json";
+	public final static String API_PSE_BPI = "http://phisix-api.appspot.com/stocks/BPI.json";
+	public final static String API_PSE_BDO = "http://phisix-api.appspot.com/stocks/BDO.json";
+	public final static String API_PSE_COL = "http://phisix-api.appspot.com/stocks/COL.json";
+	
+}
